b25955ad5982297b76d16a1c59d8d2c86b7832ea
Bolshevichok <NulliusS@yandex.ru>
2026-01-30 01:08:05 +0500

    fix for shorts

--- backend/src/services/youtube.ts
+++ backend/src/services/youtube.ts
@@ -6,12 +6,25 @@ export class YTcontent implements ContentService {
 
         const subprocess = (ytDlp as any).exec(url, {
             output: "-",
-            format: "best[ext=mp4]",
+            
+            format: "best[height<=720][ext=mp4]/bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]",
+            mergeOutputFormat: "mp4",
             noCheckCertificates: true,
             noWarnings: true,
-            preferFreeFormats: true
+            preferFreeFormats: true,
+            
+            postprocessorArgs: [
+                "Merger: -movflags frag_keyframe+empty_moov"
+            ],
+            addHeader: "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        });
+        subprocess.stderr?.on('data', (data: any) => {
+            console.error('[YT-DLP ERROR]:', data.toString());
         });
 
+        if (!subprocess.stdout) {
+             throw new Error("No stdout from yt-dlp");
+        }
         return subprocess.stdout;
     }
 }
