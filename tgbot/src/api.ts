619c6a387f0a81346d30c1f562821fddef3b8471
Bolshevichok <NulliusS@yandex.ru>
2026-02-08 21:19:14 +0500

    ok

--- tgbot/src/api.ts
+++ tgbot/src/api.ts
@@ -1,10 +1,16 @@
-import type { Readable } from "node:stream";
+import { Readable } from "node:stream";
 import config from "./config.js";
-import { configDotenv } from "dotenv";
 
-const HOST = config.host_backend
-const PORT = config.port_backend
 export async function downloadVideo(url:string): Promise<Readable | null> {
-    const file = fetch(`http://${HOST}:${PORT}/videos?`);
-
+    try {
+        const HOST = config.host_backend;
+        const PORT = config.port_backend;
+        const response = await fetch(`http://${HOST}:${PORT}/videos?url=${url}`);
+        if (!response.ok || !response.body) return null;
+        const stream = Readable.fromWeb(response.body as any);
+        return stream;
+    } catch (e){
+        console.error(e);
+        return null;
+    }
 }
--- tgbot/src/index.ts
+++ tgbot/src/index.ts
@@ -1,10 +1,22 @@
-import {Bot} from 'grammy';
-import dotenv from 'dotenv';
+import {Bot, InputFile} from 'grammy';
 import config from './config.js';
-dotenv.config({path: './.env'})
+import { downloadVideo } from './api.js';
 
 const bot = new Bot(config.botToken);
 
 bot.command("start", (ctx) => ctx.reply("Привет! Этот бот загружает ролики с YT/TT/VK. Отправь ссылку на ролик для загрузки." ));
 
-bot.on('message::url');
+bot.on('message::url', async (ctx) => {
+    try {
+        const message = ctx.message.text;
+        if (!message) return ctx.reply("ой, вей! у вас какое-то нехорошее сообщение.");
+        const video = await downloadVideo(message);
+        if (!video) return ctx.reply("ой, вей! не то ты мне шлешь. нужна только ссылка для YT/TT/VK!");
+        ctx.replyWithVideo(new InputFile(video));
+    } catch (e) {
+        console.error(e);
+        ctx.reply("кажется что-то не то...")
+    }
+});
+
+bot.start()
