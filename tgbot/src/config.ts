b0c396c33c82dc6be5b9ed456a0fa448e6a2d433
Bolshevichok <NulliusS@yandex.ru>
2026-02-10 08:23:01 +0500

    add docker and some changes with env

--- tgbot/src/config.ts
+++ tgbot/src/config.ts
@@ -1,5 +1,21 @@
 import dotenv from 'dotenv';
-dotenv.config({path: '../.env'});
+import { existsSync } from 'node:fs';
+const envPaths = ['../.env', './.env']
+let envPath: string | undefined;
+for (const path of envPaths){
+  if (existsSync(path)){
+    envPath = path;
+    break;
+  }
+}
+if (envPath){
+  dotenv.config({path: envPath});
+} else {
+  dotenv.config();
+}
+
+// dotenv.config({path: "../.env"})
+
 
 interface Config {
   port_tg: number;
